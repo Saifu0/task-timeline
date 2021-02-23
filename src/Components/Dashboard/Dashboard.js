dc027d22bae8b5932a4391190c3752d1b52df166
Saifu0 <saifurrahmankhankhan@gmail.com>
2021-02-23 13:30:13 +0530

    dummy data loaded

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { context } from '../../App';
 import ReactGantt from 'gantt-for-react';
+import dummydata from '../../data/dummydata.json';
 
 const Dashboard = () => {
 
     const Context = React.useContext(context);
     console.log(Context.timelineData.timelineData);
 
-    const tasks = Context.timelineData.timelineData;
+    // const tasks = Context.timelineData.timelineData;
+    const tasks = dummydata;
+    console.log(tasks);
 
     return (
         <div>
-            {Context.timelineData.timelineData.length>0 ? <ReactGantt
+            {/* {Context.timelineData.timelineData.length>0 ? <ReactGantt
                 tasks={tasks}
                 viewMode="Day"
-            /> : <h1>Timeline is empty! Please create some timelines.</h1>}
+            /> : <h1>Timeline is empty! Please create some timelines.</h1>} */}
+            <ReactGantt tasks={tasks} viewMode="Day" />
         </div>
     )
 }
